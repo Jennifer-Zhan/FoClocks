1f4f83e4d41e76b707cddbd540b2e2806aab6f9f
Zhan, Jennifer <zhanr@rpi.edu>
2020-10-15 03:38:32 +0800

    added add task, accumulate timer, countdown timer with php, sql, and js

---
+++ start.sql
@@ -0,0 +1,14 @@
+CREATE TABLE `onetime_task` (
+ `taskid` int(10) unsigned NOT NULL AUTO_INCREMENT,
+ `name` varchar(100) DEFAULT NULL,
+ `timer` varchar(50) DEFAULT NULL,
+ `date` date DEFAULT NULL,
+ `countdown` int(5) DEFAULT NULL,
+ `deletion` int(3) DEFAULT NULL,
+ PRIMARY KEY (`taskid`)
+);
+
+INSERT INTO onetime_task VALUES
+  (1, "Websys Project", "Countdown", '2020-10-16', 2, 0),
+  (2, "Computer Organization HW", "Countdown", '2020-10-16', 2, 0),
+  (3, "Prepare for Interview", "Accumulate", '2020-10-16',0, 0)
