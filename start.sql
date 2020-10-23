6df58907f1a0c118946b7c994f35bf58e14892d8
ruoyizhan@gmail.com <ruoyizhan@gmail.com>
2020-10-24 01:19:58 +0800

    update the command line function with task name format

--- start.sql
+++ start.sql
@@ -9,8 +9,8 @@ CREATE TABLE `onetime_task` (
 );
 
 INSERT INTO onetime_task VALUES
-  (1, "Websys Project", "Countdown", '2020-10-17', 2, 0),
-  (2, "Computer Organization HW", "Countdown", '2020-10-17', 2, 0),
-  (3, "Prepare for Interview", "Accumulate", '2020-10-17',0, 0),
-  (4, "Prepare for Websys Quiz", "Accumulate", '2020-10-18',0,0),
+  (1, "WEBSYS PROJECT", "Countdown", '2020-10-17', 2, 0),
+  (2, "COMPUTER ORGANIZATION HW", "Countdown", '2020-10-17', 2, 0),
+  (3, "PREPARE fOR INTERVIEW", "Accumulate", '2020-10-17',0, 0),
+  (4, "PREPARE fOR WEBSYS QUIZ", "Accumulate", '2020-10-18',0,0),
   (5, "FOCS HW", "Accumulate", '2020-10-18',0,0)
