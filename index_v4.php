66834e8b93bf9e08d0c2ef12e0b8123226c9e450
JensenChen <a1043904820@gmail.com>
2020-11-27 01:24:55 +0800

    update the css in index_v4.php.

---
+++ index_v4.php
@@ -0,0 +1,51 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Foclocks</title>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+	<link href="index_v2.css" rel="stylesheet" type="text/css">
+	<link href="console.css" rel="stylesheet" type="text/css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+	<script src="index.js"></script>
+	<style>
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap');
+	</style>
+</head>
+<body>
+		<div class="info_bar">
+			<section id="tag1"><a href="homepage/index.php">HOME</a></section>
+			<section id="tag2"><a href="completed%20task.html">Completed</a></section>
+			<section id="tag3"><a href="overview.html">Overview</a></section>
+			<section id="tag4"><a href="">Sign In</a></section>
+			<section id="history"><a href="">History</a></section>
+		</div>
+		<div class="todolists">
+			<div class="lists">
+
+			</div>
+		</div>
+		<div class="change_tasks">
+			<div class="change_infos">
+
+			</div>
+		</div>
+		<div class="add_tasks">
+			<div class="change_infos">
+
+			</div>
+		</div>
+		<div class="console">
+			<div class="inside_console">
+
+			</div>
+		</div>
+		<div class="timer">
+
+		</div>
+		<div class="map">
+
+		</div>
+</body>
+</html>
