ad57cfc820c6441c38bfc7e9d67b9d4bc6151ddc
JensenChen <a1043904820@gmail.com>
2020-12-05 16:57:45 +0800

    update

---
+++ index_sign.php
@@ -0,0 +1,177 @@
+<?php
+// connect the database
+$dbOk = false;
+@ $conn = new mysqli('localhost', 'root', '', 'websys_project');
+if ($conn->connect_error) {
+    echo '<div class="messages">Could not connect to the database. Error: ';
+    echo $conn->connect_errno . ' - ' . $conn->connect_error . '</div>';
+} else {
+    $dbOk = true;
+}
+
+// adding Users
+if (isset($_POST['register_form'])) {
+    if ($_POST['register_form']) {
+        $username = $_POST['username'];
+        $password = $_POST['password'];
+        $recomfirm = $_POST['recomfirm'];
+        if($password==$recomfirm){
+            $hash=password_hash($password, PASSWORD_DEFAULT);
+            $Query_AddUsers = "INSERT INTO `users` (`username`, `hash`) VALUES ('".$username."', '".$hash."')";
+            $conn->query($Query_AddUsers);
+        }
+    }
+}
+
+// sign in
+if (isset($_POST['login_form'])) {
+    if ($_POST['login_form']) {
+        $username = $_POST['username'];
+        $password = $_POST['password'];
+        $Query_hash="SELECT hash FROM users WHERE username='".$username."'";
+        $hash_execute=$conn->query($Query_hash);
+        if($hash_execute){
+            $record = $hash_execute->fetch_assoc();
+            $stored_hash=$record['hash'];
+            if(password_verify($password , $stored_hash)){
+                $Query_uid="SELECT uid FROM users WHERE username='".$username."'";
+                $uid_execute=$conn->query($Query_uid);
+                $uid_record = $uid_execute->fetch_assoc();
+                $uid=$uid_record['uid'];
+                echo $uid;
+                $Query_finduid="SELECT uid FROM profile_info WHERE uid='".$uid."'";
+                $uid_find = $conn->query($Query_finduid);
+                session_start();
+                if($uid_find->num_rows>0){
+                    $_SESSION['uid'] = $uid;
+                    echo $_SESSION['uid'];
+                    header("Location: main_page/index.php");
+
+                }
+                else{
+                    $Query_profile="INSERT INTO `profile_info` (`uid`) VALUES ('".$uid."')";
+                    $success_add=$conn->query($Query_profile);
+                    echo $success_add;
+                    $_SESSION['uid'] = $uid;
+                    echo "New account log in";
+                    header("Location: main_page/index.php");
+                }
+            }
+            else{
+                echo "UserName or Password Incorrect!";
+            }
+        }
+    }
+}
+
+?>
+
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+    <meta name="google-signin-scope" content="profile email">
+    <meta name="google-signin-client_id" content="855327976022-mgtq0odsogclg64js7t7e4agkfk5v0k2.apps.googleusercontent.com">
+    <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js?ver=3.0.1"></script>
+    <script src="https://apis.google.com/js/platform.js" async defer></script>
+    <link rel="stylesheet" href="homepage_resouces/index_sign.css">
+    <script src="homepage_resouces/index.js"></script>
+</head>
+<body>
+<!--<header>-->
+<!--  <h1>Welcome To FoClocks!</h1>-->
+<!--</header>-->
+
+<div class="login_block">
+    <!--	<div class="png2">-->
+    <!--		<img src="homepage_resouces/2.png" alt="this is a image" width="210" class="png2"/></button>-->
+    <!--	</div>-->
+    <p class="title">FoClocks<br />
+        <div class="login_infos">
+    <p class="login">Sign In</p>
+    <div class="botton_bar">
+        <p class="enter_not_login"><a href="../index_v4.php">Enter Without Login</a></p>
+        <div id="my-signin2"></div>
+        <p class="or">OR</p>
+        <form id="register_form" action="index.php" method="post">
+            <p class="user_name">Username:</p><br />
+            <input class="input_type" type="text" name="username" value="">
+            <p class="user_pw">Password:</p>
+            <input class="input_type" type="text" name="password" value="">
+            <p class="re_user_pw">Reconfirm:</p>
+            <input class="input_type" type="text" name="recomfirm" value="">
+            <input class="login_submit" type="submit" name="register_form" value="Submit">
+        </form>
+        <p class="not_have_account">Have An Account Already?</p>
+        <a class="sign_in_button" href="index.php">Now Log In</a>
+        <!--			<button type="button" id="user_login">Sign In with FoClocks Account</button>-->
+        <!--			<br/>-->
+        <!--			<button type="button" id="register">New User - Sign up a FoClocks Account</button>-->
+    </div>
+</div>
+</div>
+<div id="foclocks_login">
+    <div id="login_inner">
+        <span id="close_login">&times;</span>
+        <form class="login_form" action="index.php" method="post">
+            <h4>Log In using FoClocks account</h4>
+            <div class="input_block">
+                <span class="input_type"><br />Username: <br /></span><input type="text" name="username" value="">
+                <span class="input_type"><br />Password: <br /></span><input type="text" name="password" value=""><br /><br />
+                <input type="submit" name="login_form" value="submit">
+            </div>
+        </form>
+    </div>
+</div>
+<div id="register_block">
+    <div id="register_inner">
+        <span id="close_register">&times;</span>
+        <form id="register_form" action="index.php" method="post">
+            <h4>Sign up for a new account</h4>
+            <div class="input_block">
+                <span class="input_type"><br />Username: <br /></span><input type="text" name="username" value="">
+                <span class="input_type"><br />Password: <br /></span><input type="text" name="password" value="">
+                <span class="input_type"><br />Recomfirm Password: <br /></span><input type="text" name="recomfirm" value=""><br /><br />
+                <input type="submit" name="register_form" value="submit">
+            </div>
+        </form>
+    </div>
+</div>
+
+
+
+
+
+<!--<script>-->
+<!--  var canvas = document.getElementById("canvas");-->
+<!--  var ctx = canvas.getContext("2d");-->
+<!--  var radius = canvas.height / 2;-->
+<!--  ctx.translate(radius, radius);-->
+<!--  radius = radius * 0.90-->
+<!--  setInterval(drawClock, 1000);-->
+<!--</script>-->
+
+<script>
+  function onSuccess(googleUser) {
+    console.log('Logged in as: ' + googleUser.getBasicProfile().getName());
+  }
+  function onFailure(error) {
+    console.log(error);
+  }
+  function renderButton() {
+    gapi.signin2.render('my-signin2', {
+      'scope': 'profile email',
+      'width': 432,
+      'height': 48,
+      'longtitle': true,
+      // 'theme': 'dark',
+      'onsuccess': onSuccess,
+      'onfailure': onFailure
+    });
+  }
+</script>
+
+<script src="https://apis.google.com/js/platform.js?onload=renderButton" async defer></script>
+
+</body>
+</html>
---
+++ main_page/index_r.php
@@ -0,0 +1,170 @@
+<?php
+$dbOk = false;
+@ $db = new mysqli('localhost', 'root', '', 'websys_project');
+if ($db->connect_error) {
+    echo '<div class="messages">Could not connect to the database. Error: ';
+    echo $db->connect_errno . ' - ' . $db->connect_error . '</div>';
+} else {
+    $dbOk = true;
+}
+
+$errors = '';
+
+//get inputs from the Add infos form and insert to the database.
+if (isset($_POST['addTask'])) {
+    if ($_POST['addTask']){
+        $name = htmlspecialchars(trim($_POST["addTasksName"]));
+        $time = htmlspecialchars(trim($_POST["time_c"]));
+        $timeZone = htmlspecialchars(trim($_POST["timeZone_c"]));
+        $tag = htmlspecialchars(trim($_POST["tag_c"]));
+        $details = htmlspecialchars(trim($_POST["details_c"]));
+        session_start();
+        $uid=$_SESSION['uid'];
+        $insQuery = "insert into onetime_task (name, time, timeZone, tag, details, uid, deletion)
+	          VALUES ('".$name."', '".$time."', '".$timeZone."','".$tag ."', '".$details."','".$uid."', 0)";
+        $db->query($insQuery);
+    }
+}
+
+//get inputs from change infos and update the database if the task name matches.
+if (isset($_POST['changeTask'])) {
+    if ($_POST['changeTask']){
+        $name = htmlspecialchars(trim($_POST["changeTasksName"]));
+        $time = htmlspecialchars(trim($_POST["time"]));
+        $timeZone = htmlspecialchars(trim($_POST["timeZone"]));
+        $tag = htmlspecialchars(trim($_POST["tag"]));
+        $details = htmlspecialchars(trim($_POST["details"]));
+        if($time!=""){
+            $insQuery = "UPDATE onetime_task SET `time` = '".$time."' WHERE `name` = '".$name."'";
+            $db->query($insQuery);
+        }
+        if($timeZone!=""){
+            $insQuery = "UPDATE onetime_task SET `timeZone` = '".$timeZone."' WHERE `name` = '".$name."'";
+            $db->query($insQuery);
+        }
+        if($tag!=""){
+            $insQuery = "UPDATE onetime_task SET `tag` = '".$tag."' WHERE `name` = '".$name."'";
+            $db->query($insQuery);
+        }
+        if($details!=""){
+            $insQuery = "UPDATE onetime_task SET `details` = '".$details."' WHERE `name` = '".$name."'";
+            $db->query($insQuery);
+        }
+    }
+}
+
+if (session_status() == PHP_SESSION_NONE) {
+    session_start();
+}
+
+$uid=$_SESSION['uid'];
+$sqlTask = "select * from onetime_task where deletion = 0 AND uid=$uid";
+
+//sort the task table by time
+if(isset($_POST['sortedByTime'])){
+    if($_POST['sortedByTime']){
+        $sqlTask = 'SELECT * FROM `onetime_task` ORDER BY `time`';
+        $db->query($sqlTask);
+    }
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Foclocks</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+    <link href="index_r.css" rel="stylesheet" type="text/css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script type="text/javascript" src="index.js"></script>
+</head>
+<body>
+    <div class="left_block">
+			<div class="left_upper_block">
+				<div class="title_block">
+					<p class="title">FoClocks</p>
+					<p id="slogan">Simplify Your Life</p>
+				</div>
+				<form action="index_r.php" method="post">
+					<input class="left_block_search" name="search_block" type="text" value="" placeholder="  Search">
+					<input class="left_block_submit" name="search_submit" type="submit" value="Submit">
+					<input class="left_block_input left_block_input_1" name="all_tasks" type="button" value="All Tasks">
+					<input class="left_block_input left_block_input_2" name="today_tasks" type="button" value="Today">
+					<input class="left_block_input left_block_input_3" name="this_week_tasks" type="button" value="Week">
+					<input class="left_block_input left_block_input_4" name="history_tasks" type="button" value="History">
+				</form>
+			</div>
+			<div class="time_zone">
+
+			</div>
+			<div class="profile_block">
+				<div class="polaroid">
+					<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ_Cj7uNNkqbx3AIsEqEmYYELsqZpBScS04tg&usqp=CAU"center alt="5 Terre" style="width:100%; height: 140px; object-fit: cover; border-radius: 4px">
+					<div class="container">
+						<ion-icon name="person-circle-outline"></ion-icon>
+						<a href="tabs/profile.php" class="profile_name">Joker</a>
+					</div>
+				</div>
+			</div>
+    </div>
+		<div class="tasks_block">
+			<div class="add_block">
+				<div class="normal_add_block">
+					<input class="add_name" name="name" type="text" value="" placeholder="  Task Infos">
+					<input class="add_tag" name="tag" type="text" value="" placeholder="  Task Tag">
+					<input class="add_time" name="time" type="date" value="">
+					<input class="normal_add_submit" name="normal_add_submit" type="submit" value="Submit">
+				</div>
+				<p class="command_line_hits">Or, using command line</p>
+				<div class="command_block">
+					<input id="commandline" type="text" name="command_line" placeholder="  Command Line... Ver.0.1 (beta 1)">
+				</div>
+			</div>
+			<div class="display_tasks">
+				<p class="your_tasks">Your Tasks</p>
+					<div class="display_lists">
+						<?php
+						if ($dbOk) {
+
+								$result = $db->query($sqlTask);
+								$numRecords = $result->num_rows;
+								echo '<table>';
+								for ($i=0; $i < $numRecords; $i++) {
+										$record = $result->fetch_assoc();
+										echo '<tr>';
+										echo '<th class="TaskName">'.$record['name'].'</th>';
+										echo '<th class="WorkDate">'.$record['time'].'</th>';
+										echo '<th class="timeZone">'.$record['timeZone'].'</th>';
+										echo '<th class="tag">'.$record['tag'].'</th>';
+										echo '<th class="details">'.$record['details'].'</th>';
+										/*
+										if($record['timer']=="Countdown"){
+												echo '<th><button onclick="Countdown_timer('.$record['countdown'].')" type="button" class="TimerType">'.$record['timer'].'</button></th>';
+										}
+										else if($record['timer']=="Accumulate"){
+												echo '<th><button id="Accumulate_timer" type="button" class="TimerType">'.$record['timer'].'</button></th>';
+										}*/
+										echo '</tr>';
+								}
+								echo '</table>';
+								$result->free();
+						}
+						?>
+					</div>
+			</div>
+		</div>
+		<div class="more_infos">
+			<div class="inner_more_infos">
+				<div class="change_current_infos">
+				</div>
+				<div class="pomodoro_clocks">
+				</div>
+				<div class="famous_quotes">
+				</div>
+			</div>
+		</div>
+		<script src="https://unpkg.com/ionicons@5.2.3/dist/ionicons.js"></script>
+</body>
+</html>
